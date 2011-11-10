c657ebb07c7e9a142a4fb22bfed80f4ecbcdc9f7
Christoph Scheid <c@shri.de>
2011-11-10 10:46:14 +0100

    view prints locale_name to console for debugging

--- pyramidchameleoni18n/views.py
+++ pyramidchameleoni18n/views.py
@@ -1,7 +1,12 @@
 from pyramidchameleoni18n.models import DBSession
 from pyramidchameleoni18n.models import MyModel
 
+from pyramid.i18n import get_locale_name
+
 def my_view(request):
+    locale_name = get_locale_name(request)
+    print "DEBUG: locale_name is " + str(locale_name)
+    
     dbsession = DBSession()
     root = dbsession.query(MyModel).filter(MyModel.name==u'root').first()
     return {'root':root, 'project':'PyramidChameleonI18n'}
